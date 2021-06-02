8fe6289401d8b1c67e0c6e83e77bc6be65afa999
Akansha0211 <akansha.saxena0211@gmail.com>
2021-06-02 20:49:37 +0530

    Fetching data from differnt endpoint and displaying it inside Dynamic table

---
+++ src/comments.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Comments extends Component {
+    state = { 
+        comments:[]
+     };
+     async componentDidMount(){
+         const url = 'https://jsonplaceholder.typicode.com/comments'
+         const response = await axios.get(url)
+         const data = await response.data;
+         this.setState({comments:data})
+     }
+    render() { 
+        return ( 
+            <div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Post Id</th>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Body</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.comments.map((comment)=>{
+                            const{postId, id, name, email, body} = comment
+                            return(
+                                <tr>
+                                    <td>{comment.postId}</td>
+                                    <td>{comment.id}</td>
+                                    <td>{comment.name}</td>
+                                    <td>{comment.email}</td>
+                                    <td>{comment.body}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+         );
+    }
+}
+ 
+export default Comments;
