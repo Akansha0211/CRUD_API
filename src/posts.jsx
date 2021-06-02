b2b9370189158d820c5c0ca39e7513833f70a777
Akansha0211 <akansha.saxena0211@gmail.com>
2021-06-02 20:01:17 +0530

    Displaying the api data on the page

--- src/posts.jsx
+++ src/posts.jsx
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Posts extends Component {
-    state = {  }
+    state = { 
+        posts:[]
+     };
 
     async componentDidMount(){
         const url = "https://jsonplaceholder.typicode.com/posts";
         const response = await axios.get(url);
         const data = await response.data;
-        console.log(data);
+        this.setState({posts:data})
+        console.log(this.state.posts);
     }
     
     render() { 
         return ( 
             <div>
-                <h1>HELLO AKANSHA</h1>
+                {this.state.posts.map((post)=>{
+                    const{userId, id, title, body} = post
+                    return(
+                        <div>
+                            <p>{post.userId}</p>
+                            <p>{post.id}</p>
+                            <p>{post.title}</p>
+                            <p>{post.body}</p>
+                        </div>
+                    )
+                })}
             </div>
          );
     }
