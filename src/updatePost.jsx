6ad01811d0d89b5cf3ff1ea10b394201b24065bf
Akansha0211 <akansha.saxena0211@gmail.com>
2021-06-03 23:40:29 +0530

    UPDATE CRUD done

--- src/updatePost.jsx
+++ src/updatePost.jsx
@@ -1,26 +1,49 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 
 class UpdatePost extends Component {
-    state = {  }
+    state = { 
+        id:'',
+        title:'',
+        body:''
+     };
+    changeHandler = (e) =>{
+        this.setState({[e.target.name]:e.target.value})
+    };
+    
+    updatePost = (e)=>{
+        axios.put(`https://jsonplaceholder.typicode.com/posts/${this.state.id}`,{
+            id: this.state.id,
+            title:this.state.title,
+            body:this.state.body
+        })
+        .then(response=>{
+            console.log(response.data)
+        })
+        .catch(error=>{
+            console.log(error)
+        });
+        //STILL WORKING ON IT..
+    }
     render() { 
         return ( 
             <div className="container">
                 <div className="w-75 mx-auto shadow p-5">
                     <h2 className="text-center">Update Post</h2>
                     <div>
-                        <form>
+                        <form onSubmit={this.updatePost}>
                             <div className="form-group mb-3">
-                                <input type="number" className="form-control" placeholder="Enter Post Id" required></input>
+                                <input type="number" className="form-control" value={this.state.id} name="id" onChange={this.changeHandler} placeholder="Enter Post Id" required></input>
                             </div>
                             <div className="form-group mb-3">
-                                <textarea type="text" className="form-control" placeholder="Enter new Title" required></textarea>
+                                <textarea type="text" className="form-control" value={this.state.title} name="title" onChange={this.changeHandler} placeholder="Enter new Title" required></textarea>
                             </div>
                             <div className="form-group mb-3">
-                                <textarea type="text" className="form-control" placeholder="Enter new Body" required></textarea>
+                                <textarea type="text" className="form-control" value={this.state.body} name="body" onChange={this.changeHandler} placeholder="Enter new Body" required></textarea>
                             </div>
                             <center>
-                                <button className="btn btn-danger" onClick={this.updatePost}>Update</button>
+                                <button className="btn btn-danger">Update</button>
                             </center>
                         </form>                       
                     </div>
